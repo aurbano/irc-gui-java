15013a4d3930ad2f3bbbc389c764d471ac412983
irenemds <irene.mdes@gmail.com>
2013-04-08 17:39:47 +0200

    Interpretación de comandos

--- Proyecto IRC/src/es/uniovi/HiloSalida.java
+++ Proyecto IRC/src/es/uniovi/HiloSalida.java
@@ -20,12 +20,44 @@ public class HiloSalida extends Thread{
 			try{
 				resp = ClienteChat.netIn.remove();
 				switch(resp.type){
+				// Mostramos segun el formato especificado
+				
 					case "/MSG":
-						// Mostramos segun el formato especificado
 						if(resp.params.length > 2){
 							System.out.println(resp.params[0]+"|"+resp.params[1]+"> "+resp.params[2]);
 						}
 						break;
+						
+					case "/JOIN":
+						if(resp.params.length > 2){
+							System.out.println(">> " +resp.params[0]+" ha entrado en la sala "+resp.params[1]);
+						}
+						break;
+						
+					case "/LEAVE":
+						if(resp.params.length > 2){
+							System.out.println(">> " +resp.params[0]+" ha abandonado la sala "+resp.params[1]);
+						}
+						break;
+						
+					case "/NICK":
+						if(resp.params.length > 2){
+							System.out.println(">> " +resp.params[0]+" ha cambiado su nick a "+resp.params[2]);
+						}
+						break;
+						
+					case "/LIST":
+						System.out.println(">> Lista de salas");
+						break;
+						
+					case "/QUIT":
+						System.out.println(">> " +resp.params[0] + "ha abandonado la sala.");
+						break;
+					
+					case "/ERROR":
+						System.out.println(">> ERROR: " +resp.params[0]);
+						break;
+						
 					default:
 						System.out.println("> Unknown type: "+resp.toString());
 				}
