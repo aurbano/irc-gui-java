1db038aeb56432ef4597f51bb931c265a045b979
irenemds <irene.mdes@gmail.com>
2013-04-08 15:34:25 +0200

    Modificando texto a la clase Comando

--- Proyecto IRC/es/uniovi/HiloEntrada.java
+++ Proyecto IRC/es/uniovi/HiloEntrada.java
@@ -1,25 +1,31 @@
 package es.uniovi;
 import java.io.*;
 public class HiloEntrada {
-	String line;
+	
 	public HiloEntrada(){
 		
 	}
 
 	public void run(){
-	LineNumberReader text = new LineNumberReader (new InputStreamReader(System.in));
-	
-	while(true){
-		try {
-			
-			line = text.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
+		LineNumberReader text = new LineNumberReader (new InputStreamReader(System.in));
+		String line;
+		Comando cmd;
+		while(true){
+			try {
+				
+				line = text.readLine();
+				if (line.startsWith("/")){
+					cmd=new Comando(line.split(" "));
+				}else{
+					cmd=new Comando(new String[]{"/MSG", line});
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
 	
-	
-	
+		
 	}
 	
 }
